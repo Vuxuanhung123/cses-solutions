e38b7ce542bfa341cfaa6a399be063e4a6d08244
Vuxuanhung123 <hungvx2k9nbsd@gmail.com>
2025-07-07 10:00:50 +0700

    test

---
+++ Introductory Problems/test.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    if (fopen(".inp", "r"))
+    {
+        freopen(".inp", "r", stdin);
+        freopen(".out", "w", stdout);
+    }
+    clock_t begin = clock();
+    clock_t end = clock();
+    cerr << endl << "Time elapsed " << float(end - begin) << "s";
+}
